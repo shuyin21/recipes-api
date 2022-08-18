33e23938279714766607277f305a13ca16e14e6f
shuyin21 <shuyindev@gmail.com>
2022-08-18 19:42:34 +0100

    first commit

---
+++ backend.js
@@ -0,0 +1,31 @@
+const PORT = 8000
+const express = require('express');
+const cors = require('cors')
+const axios = require('axios')
+require('dotenv').config()
+const APP_ID = process.env.APP_ID;
+const APP_KEY = process.env.APP_KEY;
+
+const app = express();
+
+app.use(cors());
+
+app.get('/recipes', (req, res) => {
+
+
+    const options = {
+        method: 'GET',
+        url: `https://api.edamam.com/api/recipes/v2?query`,
+        params: { q: req.query.q, app_id: APP_ID, app_key: APP_KEY, type: 'public' }
+    }
+    axios.request(options).then((response) => {
+        res.json(response.data)
+    }).catch((err) => {
+        console.error(err)
+    })
+}
+)
+
+app.listen(8000, () => console.log(`Server is running on port ${PORT}`))
+
+
--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Recipe from './components/Recipe';
+import axios from 'axios';
 
 function App() {
+
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('chicken');
+
+
+
+  useEffect(() => {
+
+    const options = {
+      method: 'GET',
+      url: 'http://localhost:8000/recipes',
+      params: {
+        q: query
+      }
+    }
+    axios.request(options).then((response) => {
+
+      setRecipes(response.data.hits)
+
+    }).catch((err) => {
+      console.error(err)
+    })
+
+
+    // getRecipes();
+  }, [query])
+  // const getRecipes = async () => {
+  //   const response = await fetch(`http://localhost:8000/recipes`)
+  //   const data = await response.json()
+  //   setRecipes(data.hits);
+
+
+  // }
+
+
+
+  const updateSearch = e => {
+    e.preventDefault();
+    setSearch(e.target.value)
+    console.log(search);
+
+  }
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('')
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <form action="" className='search-form' onSubmit={getSearch}>
+        <input type="text" className='search-bar' value={search} onChange={updateSearch} />
+        <button type='submit' className='search-button' >Search</button>
+      </form>
+      <div className='recipes'>
+        {recipes.map(recipe => (
+          <Recipe key={recipe.recipe.title} title={recipe.recipe.label} calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
+
     </div>
   );
 }
